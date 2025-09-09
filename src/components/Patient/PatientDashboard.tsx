ee20fba6b87656c8e89acc4ffd262257507379d4
Sibtain Iqbal <sibtainiqbal8844@gmail.com>
2025-09-08 22:48:30 -0700

    pateint dashboard

--- src/components/Patient/PatientDashboard.tsx
+++ src/components/Patient/PatientDashboard.tsx
@@ -1,11 +1,9 @@
-export default function PatientDashboard() {
+
+
+const PatientDashboard: React.FC = () => {
   return (
-    <div className="container mx-auto px-4 py-8">
-      <h1 className="text-[150px] font-bold">Patients Dashboard</h1>
-
-
-
-      {/* Add patient-specific content here */}
-    </div>
+   <h1>hellllo patien t</h1>
   );
-}
+};
+
+export default PatientDashboard;
